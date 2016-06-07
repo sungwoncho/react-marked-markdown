d867a54c9b49b9df4c5f885b3a294617c7696e24
Vincent PARIZET <vincent.parizet@hotmail.fr>
2016-06-07 16:12:38 +0200

    Add one test

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,8 +1,13 @@
-import {expect} from 'chai';
-const {describe, it} = global;
+import React from 'react';
+import { shallow } from 'enzyme';
+import { MarkdownPreview } from '../index';
+import { expect } from 'chai';
+const { describe, it } = global;
 
-describe('sum', () => {
-  it('should add two numbers correctly', async () => {
-    expect(true).to.be.equal(true);
+describe('MarkdownPreview', () => {
+  it('should display markdown', () => {
+    const text = '# Title';
+    const wrapper = shallow(<MarkdownPreview value={text} />);
+    expect(wrapper.html()).to.be.equal('<div><h1 id="title">Title</h1>\n</div>');
   });
 });
